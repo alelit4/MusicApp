24250e87347dcad9cf6f471c3ec21dce59ce7d7a
Alelit4 <alelit4@gmail.com>
2021-01-01 21:30:31 +0000

    Update dependencies and remove basic test

--- app/src/androidTest/java/com/alexandra/musicapp/MusicAppFragmentShould.kt
+++ app/src/androidTest/java/com/alexandra/musicapp/MusicAppFragmentShould.kt
@@ -1,28 +1,17 @@
 package com.alexandra.musicapp
 
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
+
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
 import com.alexandra.musicapp.ui.MainActivity
 import org.junit.Rule
-import org.junit.Test
 import org.junit.runner.RunWith
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
 class MusicAppFragmentShould {
 
-    @Rule
-    @JvmField
-    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
-
-    @Test
-    fun load_app_name_in_fragment_catalog() {
-        val textView = onView(withId(R.id.title_music_catalog))
-        textView.check(matches(withText("MusicApp").also { isDisplayed() }))
-    }
+      // TODO Fill it!! Urgent!
 
 }
