ef17dd18d29ce5294a51524404b09574664c0474
Alelit4 <alelit4@gmail.com>
2021-01-01 12:33:15 +0000

    Hilt notation added and network permissions

---
+++ app/src/main/java/com/alexandra/musicapp/MyApplication.kt
@@ -0,0 +1,9 @@
+package com.alexandra.musicapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication: Application(){
+
+}
--- app/src/main/java/com/alexandra/musicapp/ui/MainActivity.kt
+++ app/src/main/java/com/alexandra/musicapp/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.alexandra.musicapp
+package com.alexandra.musicapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,8 +7,11 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.alexandra.musicapp.R
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
