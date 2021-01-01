4695653777556c711a56ca6c5ddbce8b037f8b64
Alelit4 <alelit4@gmail.com>
2021-01-01 21:40:52 +0000

    Generic utils added

---
+++ app/src/main/java/com/alexandra/musicapp/utils/Constants.kt
@@ -0,0 +1,12 @@
+package com.alexandra.musicapp.utils
+
+class Constants {
+    companion object{
+        const val BASE_URL = "https://itunes.apple.com"
+        const val QUERY_TERM = "term"
+        const val QUERY_ENTITY = "entity"
+        const val QUERY_ATTRIBUTE = "attribute"
+        const val ALL_ARTIST = "allArtist"
+        const val ALL_ARTIST_TERM = "allArtistTerm"
+    }
+}
